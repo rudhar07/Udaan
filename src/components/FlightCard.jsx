14f7c892e4e639f991f61116904d77fb7498c5a2
Rudhar <rudharbajaj@gmail.com>
2025-04-20 03:17:55 +0530

    favourite button update

--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -116,24 +116,32 @@ function FlightCard({ flight }) {
             <div className="relative">
               <button 
                 onClick={handleFavoriteToggle}
-                className={`mr-4 p-2 rounded-full transition-colors ${
+                className={`mr-4 p-2 rounded-full transition-colors hover-heart ${
                   isFavorite 
                     ? 'bg-red-100 text-red-500 hover:bg-red-200' 
-                    : 'bg-slate-100 text-slate-400 hover:text-indigo-500 hover:bg-indigo-50'
+                    : 'bg-white text-slate-400 hover:text-slate-600 border border-slate-200 hover:border-slate-300'
                 }`}
                 aria-label={isFavorite ? "Remove from favorites" : "Add to favorites"}
                 title={isFavorite ? "Remove from favorites" : "Add to favorites"}
               >
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path 
-                    fillRule="evenodd" 
-                    d={isFavorite 
-                      ? "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" 
-                      : "M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656zm6.828 1.172a4 4 0 00-5.656 0l-1.172 1.171-1.172-1.171a4 4 0 00-5.656 5.656l6.828 6.829 6.828-6.829a4 4 0 000-5.656z"
-                  } 
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                {isFavorite ? (
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path 
+                      fillRule="evenodd" 
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" 
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                ) : (
+                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path 
+                      strokeLinecap="round" 
+                      strokeLinejoin="round" 
+                      strokeWidth="2" 
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    ></path>
+                  </svg>
+                )}
               </button>
               
               {showLoginPrompt && !currentUser && (
--- src/context/FlightContext.jsx
+++ src/context/FlightContext.jsx
@@ -1,3 +1,4 @@
+
 import { createContext, useState, useContext, useEffect } from 'react';
 import amadeusService from '../services/amadeus';
 import { 
